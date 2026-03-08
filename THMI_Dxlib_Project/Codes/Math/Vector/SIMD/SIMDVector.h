755c7f9052f45f1be98d047f7e582ba5f7466211
MDAIKI1230 <shimamizu1230@gmail.com>
2026-03-09 07:44:18 +0900

    SIMDVectorシャッフル関数、slpatコンストラクタ追加

--- THMI_Dxlib_Project/Codes/Math/Vector/SIMD/SIMDVector.h
+++ THMI_Dxlib_Project/Codes/Math/Vector/SIMD/SIMDVector.h
@@ -4,7 +4,7 @@
 
 // 128bit SIMDレジスタラッパー構造体なので、ヘッダ内で収める
 
-struct SIMDVector
+struct alignas(16) SIMDVector
 {
 public:
 	__m128 value;
@@ -29,7 +29,12 @@ public:
 	/// <param name="_z">値3</param>
 	/// <param name="_w">値4</param>
 	SIMDVector(float _x, float _y, float _z, float _w) :
-		value{ _mm_setr_ps(_x,_y,_z,_w) }
+		value{ _mm_set_ps(_w,_z,_y,_x) }
+	{}
+
+	// splat
+	SIMDVector(float v):
+		value{ _mm_set1_ps(v) }
 	{}
 
 	/// <summary>
@@ -39,6 +44,21 @@ public:
 	void Store(float* _ptr)const
 	{
 		_mm_store_ps(_ptr, value);
+	}
+
+	/// <summary>
+	/// 値入れ替え
+	/// </summary>
+	/// <typeparam name="X">値1がどこと変わるか</typeparam>
+	/// <typeparam name="Y">値1がどこと変わるか</typeparam>
+	/// <typeparam name="Z">値1がどこと変わるか</typeparam>
+	/// <typeparam name="W">値1がどこと変わるか</typeparam>
+	/// <param name="_value">入れ替える元の値</param>
+	/// <returns>入れ替えられた新しい値</returns>
+	template<int X,int Y,int Z,int W>
+	static SIMDVector Shuffle(const SIMDVector& _value)
+	{
+		return _mm_shuffle_ps(_value.value, _value.value, _MM_SHUFFLE(W, Z, Y, X));
 	}
 
 	// 加算
