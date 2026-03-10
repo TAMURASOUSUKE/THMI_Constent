8f07d7d0e0f70635695a8218e90f5cabb2788cdd
MDAIKI1230 <shimamizu1230@gmail.com>
2026-03-10 10:13:10 +0900

    Vector2定義仮

--- THMI_Dxlib_Project/Codes/Math/Vector/Vector2/Vector2.h
+++ THMI_Dxlib_Project/Codes/Math/Vector/Vector2/Vector2.h
@@ -15,16 +15,17 @@ public:
 	static constexpr Vector2<T> LEFT{ -1.0,0.0 };
 	static constexpr Vector2<T> RIGHT{ 1.0,0.0 };
 public:
-	// アクセス用数値
 	union
 	{
+		// アクセス用数値
 		struct 
 		{
 			float x;
 			float y;
 		};
 
-		SIMDVector value;
+		// SIMD計算用
+		SIMDVector simd;
 	};
 
 public:
@@ -34,64 +35,64 @@ public:
 	/// <param name="_x">X</param>
 	/// <param name="_y">Y</param>
 	Vector2(T _x, T _y) :
-		value(_x,_y,0,0)
+		simd(_x,_y,0,0)
 	{}
 
 	// 加算
-	Vector2<T> operator+(const Vector2<T>& other)const;
+	Vector2<T> operator+(const Vector2<T>& _other)const;
 	// 減算
-	Vector2<T> operator-(const Vector2<T>& other)const;
+	Vector2<T> operator-(const Vector2<T>& _other)const;
 	// 乗算
-	Vector2<T> operator*(float value)const;
+	Vector2<T> operator*(float _value)const;
 	/// <summary>
 	/// 徐算(整数の場合static_castで整数になります)
 	/// </summary>
 	/// <returns>0徐算の場合は、そのままの値を返します</returns>
-	Vector2<float> operator/(float value)const;
+	Vector2<T> operator/(float _value)const;
 
 	// 加算
-	Vector2<T>& operator+=(const Vector2<T>& other);
+	Vector2<T>& operator+=(const Vector2<T>& _other);
 	// 減算
-	Vector2<T>& operator-=(const Vector2<T>& other);
+	Vector2<T>& operator-=(const Vector2<T>& _other);
 	// 乗算
-	Vector2<T>& operator*=(float value);
+	Vector2<T>& operator*=(float _value);
 	/// <summary>
 	/// 徐算(整数の場合static_castで整数になります)
 	/// </summary>
 	/// <returns>0徐算の場合は、そのままの値を返します</returns>
-	Vector2<T>& operator/=(float value);
+	Vector2<T>& operator/=(float _value);
 
 	// 逆ベクトル
 	Vector2<T> operator-()const;
 
 	// 等価
-	bool operator==(const Vector2& other) const;
+	bool operator==(const Vector2<T>& _other) const;
 	// 不等価
-	bool operator!=(const Vector2& other) const;
+	bool operator!=(const Vector2<T>& _other) const;
 
 	// ベクトルの長さ(実数のみ)
-	float Length() const requires std::is_floating_point<T>;
+	float Length() const requires std::is_floating_point_v<T>;
 	// ベクトルの長さの2乗(実数のみ)
-	float LengthSqr() const requires std::is_floating_point<T>;
+	float LengthSqr() const requires std::is_floating_point_v<T>;
 	// 2つのベクトル間の距離(実数のみ)
-	static float Distance(const Vector2& from, const Vector2& to) requires std::is_floating_point<T>;
+	static float Distance(const Vector2<T>& _from, const Vector2<T>& _to) requires std::is_floating_point_v<T>;
 	// 2つのベクトル間の距離の2乗(実数のみ)
-	static float DistanceSqr(const Vector2& from, const Vector2& to) requires std::is_floating_point<T>;
+	static float DistanceSqr(const Vector2<T>& _from, const Vector2<T>& _to) requires std::is_floating_point_v<T>;
 	// 2つのベクトル間の角度(実数のみ)
-	static float Angle(const Vector2& from, const Vector2& to) requires std::is_floating_point<T>;
+	static float Angle(const Vector2<T>& _from, const Vector2<T>& _to) requires std::is_floating_point_v<T>;
 
-	// 正規化(値の変化なし)(実数のみ)
-	Vector2<float> Normalize() requires std::is_floating_point<T>;
-	// 正規化(値の変化なし)(実数のみ)
-	static Vector2<float> Normalize(const Vector2& vec) requires std::is_floating_point<T>;
+	// 正規化(値の変化有)(実数のみ)
+	Vector2<T>& Normalize() requires std::is_floating_point_v<T>;
+	// 正規化(値の変化有)(実数のみ)
+	static Vector2<T>& Normalize(const Vector2<T>& _vec) requires std::is_floating_point_v<T>;
 
-	// 正規化(値の変化あり)(実数のみ)
-	Vector2<float> Normalized() requires std::is_floating_point<T>;
-	// 正規化(値の変化あり)(実数のみ)
-	static Vector2<float> Normalized(const Vector2& vec) requires std::is_floating_point<T>;
+	// 正規化(値の変化無)(実数のみ)
+	Vector2<T> Normalized() requires std::is_floating_point_v<T>;
+	// 正規化(値の変化無)(実数のみ)
+	static Vector2<T> Normalized(const Vector2<T>& _vec) requires std::is_floating_point_v<T>;
 
 	// 内積
-	static float Dot(const Vector2& vec1, const Vector2& vec2);
+	static float Dot(const Vector2<T>& _vec1, const Vector2<T>& _vec2);
 	// 外積
-	static float Cross(const Vector2& vec1, const Vector2& vec2);
+	static float Cross(const Vector2<T>& _vec1, const Vector2<T>& _vec2);
 };
