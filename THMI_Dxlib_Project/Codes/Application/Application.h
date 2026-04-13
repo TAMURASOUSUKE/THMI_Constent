4582ac537ffd106ec61e037539260c9b2f98a59f
TAMURASOUSUKE <kokotomo.88934@gmail.com>
2026-04-14 03:28:46 +0900

    ApplicationFileのヘッダ宣言を完了

--- THMI_Dxlib_Project/Codes/Application/Application.h
+++ THMI_Dxlib_Project/Codes/Application/Application.h
@@ -4,5 +4,28 @@
 class Application
 {
 public:
-	
+	// コンストラクタ
+	Application();
+	// デストラクタ : Dxlibの終了処理等を受け持つ
+	~Application();
+
+	// コピーコンストラクタの禁止
+	Application(const Application& _other) = delete;
+	// コピー代入の禁止
+	Application& operator =(const Application& _other) = delete;
+
+	// ウィンドウの設定や3D関連の設定、Dxlibの初期化を行う
+	bool Initialize();
+
+	// ゲームループを行う
+	void Run();
+private:
+	// 入力やオブジェクト更新
+	void Update();
+	// 画面のクリアやオブジェクト描画、画面フリップを行う
+	void Draw();
+
+private:
+
+
 };
