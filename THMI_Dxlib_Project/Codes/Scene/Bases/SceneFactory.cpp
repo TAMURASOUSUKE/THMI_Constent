33493653cbf8764d72be179e69edbe3262bfc9e0
TAMURASOUSUKE <kokotomo.88934@gmail.com>
2026-04-14 05:17:37 +0900

    田村デバッグシーン作成用関数の追加

--- THMI_Dxlib_Project/Codes/Scene/Bases/SceneFactory.cpp
+++ THMI_Dxlib_Project/Codes/Scene/Bases/SceneFactory.cpp
@@ -15,6 +15,7 @@ void SceneFactory::Initialize()
 	creationMap[SceneType::ChooseDebug] = []() { return std::make_unique<HashinoDebugScene>(); }; // 橋野デバッグシーンを作成する
 	creationMap[SceneType::ChooseDebug] = []() { return std::make_unique<ItouDebugScene>(); }; // 伊藤デバッグシーンを作成する
 	creationMap[SceneType::ChooseDebug] = []() { return std::make_unique<MizushimaDebugScene>(); }; // 水島デバッグシーンを作成する
+	creationMap[SceneType::ChooseDebug] = []() { return std::make_unique<TamuraDebugScene>(); }; // 田村デバッグシーンを作成する
 }
 
 std::unique_ptr<SceneBase> SceneFactory::CreateScene(SceneType _type)
