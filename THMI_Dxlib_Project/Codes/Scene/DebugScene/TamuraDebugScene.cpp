7390a3cfc8bbb7d13cb149c4440d4e8ef5cdb670
TAMURASOUSUKE <kokotomo.88934@gmail.com>
2026-04-14 14:27:01 +0900

    自分のシーンが正しく動くかのテスト

--- THMI_Dxlib_Project/Codes/Scene/DebugScene/TamuraDebugScene.cpp
+++ THMI_Dxlib_Project/Codes/Scene/DebugScene/TamuraDebugScene.cpp
@@ -5,6 +5,7 @@ void TamuraDebugScene::Draw()
 {
 	// 描画用処理
 	DrawString(0, 0, "ここは田村シーンです", GetColor(255, 255, 255), GetColor(255, 255, 255));
+	DrawString(0, 20, "テストテスト", GetColor(255, 255, 255), GetColor(255, 255, 255));
 }
 
 void TamuraDebugScene::Initialize()
