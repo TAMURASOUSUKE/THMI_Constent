b84ee8bbc21a411478bfc11ee841f38cd632c02f
MDAIKI1230 <shimamizu1230@gmail.com>
2026-04-15 15:25:01 +0900

    MatGenerateFunc宣言

--- THMI_Dxlib_Project/Codes/Math/Matrix/MatGenerateFunc.h
+++ THMI_Dxlib_Project/Codes/Math/Matrix/MatGenerateFunc.h
@@ -1,2 +1,16 @@
 #pragma once
 
+#include "Matrix.h"
+#include "../Quaternion/Quaternion.h"
+
+namespace MatGenerateFunc
+{
+	Matrix4x4 Translate(const Vector3& _vec);
+	Matrix4x4 Scale(const Vector3& _scale);
+	Matrix4x4 Rotate(const Quaternion& _rot);
+	Matrix4x4 RotateX(float _x);
+	Matrix4x4 RotateY(float _y);
+	Matrix4x4 RotateZ(float _z);
+	Matrix4x4 TRS(const Vector3& _vec, const Quaternion& _rot, const Vector3& _scale);
+	Matrix4x4 LookAt(const Vector3& eye, const Vector3& target, const Vector3& up);
+}
