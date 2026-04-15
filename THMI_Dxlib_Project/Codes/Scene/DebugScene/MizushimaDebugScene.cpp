67c410b8cbb0a817bace5a7f98022b2c518164b5
TAMURASOUSUKE <kokotomo.88934@gmail.com>
2026-04-15 15:35:04 +0900

    各シーンに統合

--- THMI_Dxlib_Project/Codes/Scene/DebugScene/MizushimaDebugScene.cpp
+++ THMI_Dxlib_Project/Codes/Scene/DebugScene/MizushimaDebugScene.cpp
@@ -1,20 +1,20 @@
 ﻿#include <Dxlib.h>
+#include "../Object/Bases/ObjectFactory.h"
 #include "MizushimaDebugScene.h"
 
-void MizushimaDebugScene::Draw()
-{
-	// 描画用処理
-	DrawString(0, 0, "ここは水島シーンです", GetColor(255, 255, 255), GetColor(255, 255, 255));
-}
 
 void MizushimaDebugScene::Initialize()
 {
+	ObjectFactory::BindManager(objManager); // シーンのオブジェクトマネージャーをバインド
+
+	objManager.SortUI(); // UIをソートする
+
 	currentStep = SceneStep::Execute; // 実行処理へ
 }
 
 void MizushimaDebugScene::Execute()
 {
-
+	objManager.Update(); // 更新処理
 
 	// Enterを押すことでシーンを抜ける処理へ
 	if (CheckHitKey(KEY_INPUT_RETURN))
@@ -26,10 +26,20 @@ void MizushimaDebugScene::Execute()
 void MizushimaDebugScene::FixedExecute()
 {
 	// 固定更新用処理
+	objManager.FixedUpdate();
 }
 
 SceneType MizushimaDebugScene::Terminate()
 {
 	// シーンを抜ける際の処理
 	return SceneType::ChooseDebug; // 選択シーンに戻すようにしていますがプロジェクトの進行に合わせて変える可能性があります。
+}
+
+void MizushimaDebugScene::Draw()
+{
+	objManager.Draw();
+
+
+	// 描画用処理
+	DrawString(0, 0, "ここは水島シーンです", GetColor(255, 255, 255), GetColor(255, 255, 255));
 }
--- THMI_Dxlib_Project/Codes/Scene/DebugScene/TamuraDebugScene.cpp
+++ THMI_Dxlib_Project/Codes/Scene/DebugScene/TamuraDebugScene.cpp
@@ -1,20 +1,21 @@
 ﻿#include <Dxlib.h>
 #include "TamuraDebugScene.h"
+#include "../Object/Bases/ObjectFactory.h"
 
-void TamuraDebugScene::Draw()
-{
-	// 描画用処理
-	DrawString(0, 0, "ここは田村シーンです", GetColor(255, 255, 255), GetColor(255, 255, 255));
-}
 
 void TamuraDebugScene::Initialize()
 {
+	ObjectFactory::BindManager(objManager); // シーンのオブジェクトマネージャーをバインド
+
+	objManager.SortUI(); // UIをソートする
+
 	currentStep = SceneStep::Execute; // 実行処理へ
 }
 
 void TamuraDebugScene::Execute()
 {
 
+	objManager.Update(); // 更新処理
 
 	// Enterを押すことでシーンを抜ける処理へ
 	if (CheckHitKey(KEY_INPUT_RETURN))
@@ -26,10 +27,19 @@ void TamuraDebugScene::Execute()
 void TamuraDebugScene::FixedExecute()
 {
 	// 固定更新用処理
+	objManager.FixedUpdate();
 }
 
 SceneType TamuraDebugScene::Terminate()
 {
 	// シーンを抜ける際の処理
 	return SceneType::ChooseDebug; // 選択シーンに戻すようにしていますがプロジェクトの進行に合わせて変える可能性があります。
+}
+
+void TamuraDebugScene::Draw()
+{
+	objManager.Draw();
+
+	// 描画用処理
+	DrawString(0, 0, "ここは田村シーンです", GetColor(255, 255, 255), GetColor(255, 255, 255));
 }
