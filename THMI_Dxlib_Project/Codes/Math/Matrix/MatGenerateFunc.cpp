4da8fca1b8274b8cca50f89f27cbfb2d7bb07600
MDAIKI1230 <shimamizu1230@gmail.com>
2026-04-16 14:33:17 +0900

    MatGenerateFuncの回転系以外

--- THMI_Dxlib_Project/Codes/Math/Matrix/MatGenerateFunc.cpp
+++ THMI_Dxlib_Project/Codes/Math/Matrix/MatGenerateFunc.cpp
@@ -1,2 +1,51 @@
 #include "MatGenerateFunc.h"
 
+Matrix4x4 MatGenerateFunc::Translate(const Vector3& _vec)
+{
+	return Matrix4x4{
+		1.0f,0.0f,0.0f,_vec.x,
+		0.0f,1.0f,0.0f,_vec.y,
+		0.0f,0.0f,1.0f,_vec.z,
+		0.0f,0.0f,0.0f,1.0f,
+	};
+}
+
+Matrix4x4 MatGenerateFunc::Scale(const Vector3& _scale)
+{
+	return Matrix4x4{
+		_scale.x,0.0f,0.0f,0.0f,
+		0.0f,_scale.y,0.0f,0.0f,
+		0.0f,0.0f,_scale.x,0.0f,
+		0.0f,0.0f,0.0f,1.0f,
+	};
+}
+
+Matrix4x4 MatGenerateFunc::Rotate(const Quaternion& _rot)
+{
+
+}
+
+Matrix4x4 MatGenerateFunc::RotateX(float _rad)
+{
+
+}
+
+Matrix4x4 MatGenerateFunc::RotateY(float _rad)
+{
+
+}
+
+Matrix4x4 MatGenerateFunc::RotateZ(float _rad)
+{
+
+}
+
+Matrix4x4 MatGenerateFunc::TRS(const Vector3& _vec, const Quaternion& _rot, const Vector3& _scale)
+{
+	return Translate(_vec) * Rotate(_rot) * Scale(_scale);
+}
+
+Matrix4x4 MatGenerateFunc::LookAt(const Vector3& _eye, const Vector3& _target, const Vector3& _up)
+{
+
+}
--- THMI_Dxlib_Project/Codes/Math/Matrix/MatGenerateFunc.h
+++ THMI_Dxlib_Project/Codes/Math/Matrix/MatGenerateFunc.h
@@ -8,9 +8,9 @@ namespace MatGenerateFunc
 	Matrix4x4 Translate(const Vector3& _vec);
 	Matrix4x4 Scale(const Vector3& _scale);
 	Matrix4x4 Rotate(const Quaternion& _rot);
-	Matrix4x4 RotateX(float _x);
-	Matrix4x4 RotateY(float _y);
-	Matrix4x4 RotateZ(float _z);
+	Matrix4x4 RotateX(float _rad);
+	Matrix4x4 RotateY(float _rad);
+	Matrix4x4 RotateZ(float _rad);
 	Matrix4x4 TRS(const Vector3& _vec, const Quaternion& _rot, const Vector3& _scale);
 	Matrix4x4 LookAt(const Vector3& _eye, const Vector3& _target, const Vector3& _up);
 }
