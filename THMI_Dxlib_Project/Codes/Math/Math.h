c69ecb531bb81214c65ff2bf995272ebd75b0834
MDAIKI1230 <shimamizu1230@gmail.com>
2026-04-20 14:36:51 +0900

    Math.h作成

---
+++ THMI_Dxlib_Project/Codes/Math/Math.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "Matrix/Matrix.h"
+#include "Quaternion/Quaternion.h"
+#include "Vector/Vector2/Vector2.h"
+#include "Vector/Vector2/Vector2Int.h"
+#include "Vector/Vector3/Vector3.h"
+#include "Vector/Vector3/Vector3Int.h"
+#include "Transform/Transform.h"
+#include "Matrix/MatGenerateFunc.h"
